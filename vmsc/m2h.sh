27a320622105b72b063bc46cbcaa41ea7395db5a
pecodrive <pecodrive@gmail.co>
2015-11-21 22:08:28 +0900

    add

---
+++ vmsc/m2h.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#  _    ___      __              ______                __  ___                  __           
+# | |  / (_)____/ /___  ______ _/ / __ )____  _  __   /  |/  /___  __  ______  / /____  _____
+# | | / / / ___/ __/ / / / __ `/ / __  / __ \| |/_/  / /|_/ / __ \/ / / / __ \/ __/ _ \/ ___/
+# | |/ / / /  / /_/ /_/ / /_/ / / /_/ / /_/ />  <   / /  / / /_/ / /_/ / / / / /_/  __/ /    
+# |___/_/_/   \__/\__,_/\__,_/_/_____/\____/_/|_|  /_/  /_/\____/\__,_/_/ /_/\__/\___/_/     
+#
+# VirtualBox自動起動及び自動sshfsマウントスクリプト
+# 2015.7.29 
+
+. ~/vmsrc.sh
+
+white_green="\e[37;42;1m"
+white_red="\e[37;41;1m"
+colorEnd="\e[m"
+
+#引数確認
+if [ $# -eq 0 ]; then
+    /bin/echo -e "${white_red}VM名を指定してください${colorEnd}"
+    exit 1
+fi
+
+vmname=$1
+
+VBoxManage list vms | grep "$1";isvms=$?
+if [ $isvms -ne 0 ]; then
+    /bin/echo -e "${white_red}指定されたVM名は存在しません${colorEnd}"
+    /bin/echo -e "${white_red}存在するVM名は以下です${colorEnd}"
+    VBoxManage list vms
+    exit 1
+fi
+
+#起動確認
+VBoxManage list runningvms | grep "$1";isRunning=$?
+if [ $isRunning -eq 0 ]; then
+    /bin/echo -e "${white_red}$1はすでに起動しています${colorEnd}"
+    exit 1
+fi
+
+
+
+#VM起動
+VBoxManage startvm "$1" --type headless > /dev/null 2>&1
+/bin/echo -e "${white_green}VM起動${colorEnd}"
+#OSが起動するまでに約１分かかるのでウエイトを挟む
+e=0
+/bin/echo -e "${white_green}OS起動待機中(60秒待ちます)${colorEnd}"
+for e in 0 5 10 15 20 25 30 35 40 45 50 55 60; do
+    echo -n "$e秒…\r" 
+    sleep 5s
+done
+
+#sshfsマウント処理
+count=0
+/bin/echo -e "${white_green}マウント確認中${colorEnd}"
+until eval '$'${vmname}
+do
+    echo -n "…"
+    sleep 5s	
+    count=$((count+1))
+    if [ $count -gt 20 ]; then
+	/bin/echo -e "${white_red}20回試行したがダメでした${colorEnd}"
+	exit 255
+    fi    
+done 
+exit 0
---
+++ vmsc/um2h.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+#| |  / (_)____/ /___  ______ _/ / __ )____  _  __   / / / /___  /  |/  /___  __  ______  / /____  _____
+#| | / / / ___/ __/ / / / __ `/ / __  / __ \| |/_/  / / / / __ \/ /|_/ / __ \/ / / / __ \/ __/ _ \/ ___/
+#| |/ / / /  / /_/ /_/ / /_/ / / /_/ / /_/ />  <   / /_/ / / / / /  / / /_/ / /_/ / / / / /_/  __/ /    
+#|___/_/_/   \__/\__,_/\__,_/_/_____/\____/_/|_|   \____/_/ /_/_/  /_/\____/\__,_/_/ /_/\__/\___/_/     
+#
+# sshfsアンマウント及びVMのシャットダウンスクリプト
+#
+# 2015.7.29
+
+white_green="\e[37;42;1m"
+white_red="\e[37;41;1m"
+colorEnd="\e[m"
+
+#引数確認
+if [ $# -eq 0 ]; then
+    /bin/echo -e "${white_red}VM名を指定してください${colorEnd}"
+    exit 1
+fi
+
+#起動確認
+VBoxManage list runningvms | grep "$1";isRunning=$?
+if [ $isRunning -ne 0 ]; then
+    /bin/echo -e "${white_red}$1は起動していません${colorEnd}"
+    exit 1
+fi
+
+fusermount -u -z /home/pecodrive/Documents/project/mounter/ > /dev/null 2>&1;isunmount=$?
+if [ $isunmount -ne 0 ]; then
+    /bin/echo -e "${white_red}アンマウント失敗${colorEnd}"
+elif [ $isunmount -eq 0 ]; then
+    /bin/echo -e "${white_green}アンマウント成功${colorEnd}"
+fi
+
+VBoxManage controlvm $1 poweroff >/dev/null 2>&1
+/bin/echo -e "${white_green}VMをシャットダウンしました${colorEnd}"
+
+exit 0
